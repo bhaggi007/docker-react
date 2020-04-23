a3244dea2a0e0814c46124d0c5a88f32472bf4e1
klaus <devgrimoire@gmail.com>
2020-04-23 16:48:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I dont have to rebuild the image to show on front end
         </p>
         <a
           className="App-link"
